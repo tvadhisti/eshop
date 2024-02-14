c32296eaf4295a97bf32be812d643e47d40135c1
adhis <tiva.adhisti@ui.ac.id>
2024-02-15 03:13:07 +0700

    productservice updated

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -3,9 +3,9 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 public interface ProductService {
-    public Product create(Product product);
-    public List<Product> findAll();
-    public Product getProductId(String id);
-    public void deleteProductById(String id);
-    public Product editProduct(Product product);
+    Product create(Product product);
+    List<Product> findAll();
+    Product getProductId(String id);
+    void deleteProductById(String id);
+    Product editProduct(Product product);
 }
