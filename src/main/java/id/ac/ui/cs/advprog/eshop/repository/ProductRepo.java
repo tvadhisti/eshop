bbd0389fce26c8e87e9b284e0a34d6385e2b0855
adhis <tiva.adhisti@ui.ac.id>
2024-02-24 20:19:47 +0700

    unused import deleted

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepo.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepo.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 
 import java.util.Iterator;
-import java.util.List;
 
 public interface ProductRepo {
     Product create(Product product);
