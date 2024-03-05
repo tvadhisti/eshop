a9bef269891d7408a2ab04383a3c157d91fd9ccc
adhis <tiva.adhisti@ui.ac.id>
2024-03-05 16:31:24 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImpl.java
@@ -3,6 +3,7 @@ import id.ac.ui.cs.advprog.eshop.model.Order;
 import id.ac.ui.cs.advprog.eshop.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.NoSuchElementException;
 
 import java.util.List;
 
@@ -10,24 +11,32 @@ import java.util.List;
 public class OrderServiceImpl implements OrderService {
     @Autowired
     private OrderRepository orderRepository;
-
     @Override
     public Order createOrder(Order order) {
+        if (orderRepository.findById(order.getId()) == null) {
+            orderRepository.save(order);
+            return order;
+        }
         return null;
     }
-
     @Override
     public Order updateStatus(String orderId, String status) {
-        return null;
+        Order order = orderRepository.findById(orderId);
+        if (order != null) {
+            Order newOrder = new Order(order.getId(), order.getProducts(),
+                    order.getOrderTime(), order.getAuthor(), status);
+            orderRepository.save(newOrder);
+            return newOrder;
+        } else {
+            throw new NoSuchElementException();
+        }
     }
-
     @Override
     public List<Order> findAllByAuthor(String author) {
-        return null;
+        return orderRepository.findAllByAuthor(author);
     }
-
     @Override
     public Order findById(String orderId) {
-        return null;
+        return orderRepository.findById(orderId);
     }
 }
--- src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/service/OrderServiceImplTest.java
@@ -58,12 +58,11 @@ public class OrderServiceImplTest {
     @Test
     void testCreateOrderIfAlreadyExists() {
         Order order = orders.get(1);
-        doReturn(order).when(orderRepository).save(order);
+        doReturn(order).when(orderRepository).findById(order.getId());
 
         assertNull(orderService.createOrder(order));
         verify(orderRepository, times(0)).save(order);
     }
-
     @Test
     void testUpdateStatus() {
         Order order = orders.get(1);
