b03e0a632ca3b1aba1e34f063e217a23595159d3
adhis <tiva.adhisti@ui.ac.id>
2024-03-07 00:31:52 +0700

    Add PaymentStatus enum and Apply PaymentStatus enum check to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,38 +1,36 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import java.util.Map;
+
+import enums.PaymentStatus;
+
 import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
+
+@Getter
+
 public class Payment {
-    private String id;
-    private String method;
-    @Setter
-    private String status;
-    private Map<String, String> paymentData;
+    String id;
+    String method;
+    String status;
+    Map<String, String> paymentData;
 
-
-    public Payment(String id, String method, Map<String, String> paymentData, String status) {
+    public Payment(String id, String method, Map<String, String> paymentData) {
         this.id = id;
         this.method = method;
         this.paymentData = paymentData;
-        this.status = status;
     }
 
-    public String getId() {
-        return id;
+    public Payment(String id, String method, Map<String, String> paymentData, String status) {
+        this(id, method, paymentData);
+        this.setStatus(status);
     }
 
-    public String getMethod() {
-        return method;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public Map<String, String> getPaymentData() {
-        return paymentData;
+    public void setStatus(String status) {
+        if (PaymentStatus.contains(status)) {
+            this.status = status;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
